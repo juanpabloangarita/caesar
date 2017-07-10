3e1b3e2f765e5c7e005305a5c1326651c38265b2
juanpabloangarita <juanpabloangarita@gmail.com>
2017-07-10 14:34:03 +0200

    enhanced with vince's formula

--- caesar.rb
+++ caesar.rb
@@ -13,24 +13,19 @@ def crypto (str, key)
 	str.each_byte do |x|
 		key = key%26
 		if x < 65 || (x>90 && x<97) || x>122
-		print x.chr
-		else
-			if (x+key) >122
-				print (((x+key)-122)+96).chr
-			elsif x>96 && x+key >97
-				print (x+key).chr
-			end
-			
-
-
+		  print x.chr
+		elsif (x>96 && x<123 && (x+key)>122) || (x>64 && x<91 && (x+key)>90)
+		  print ((x+key)-26).chr
+		elsif  
+		  print (x+key).chr
 		end
 	end
 puts ""
 end
 
 
-crypto('yyy', 55)
-crypto('aaa', 3)
+crypto('YYY', 3)
+crypto('aAA', 3)
 crypto('098345 a b', 2)
 
 
